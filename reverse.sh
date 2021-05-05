0a7c26ac93db81ad12c31415e5a9127f36763bcc
Abhirami C S <83262450+Abhiramics@users.noreply.github.com>
2021-05-05 09:46:59 +0530

    Update reverse.sh

--- reverse.sh
+++ reverse.sh
@@ -1,15 +1,12 @@
-function prime()
-{
-    for((i=2; i<=num; i++))
-    do
-        if [ `expr $num % $i` == 0 ]
-        then
-            echo $num is not prime
-            exit
-        fi
-    done
-    echo $num is a prime number
-}
-
-read num;
-prime "$num";
+echo -n "enter number : "
+read n 
+sd=0
+rev=""
+on=$n
+while [ $n -gt 0 ]
+do
+     sd=$(( $n % 10 ))
+     n=$(( $n / 10 ))
+     rev=$( echo ${rev}${sd})
+done
+echo "$rev is a reverse order of $on"
